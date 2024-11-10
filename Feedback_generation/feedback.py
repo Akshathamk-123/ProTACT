2032ee426d940a9129a3a2df332fcb1df0847add
Adrija Banerjee <92499352+adrija-banerjee@users.noreply.github.com>
2024-11-11 01:33:10 +0530

    Update feedback.py

--- Feedback_generation/feedback.py
+++ Feedback_generation/feedback.py
@@ -21,7 +21,7 @@ class EssayFeedbackGenerator:
         # Initialize the fine-tuned OpenAI model
         self.openai_llm = OpenAI(
             model_name="ft:gpt-3.5-turbo-1106:pes:finetune-feedback:AQwJEq7T:ckpt-step-800",
-            openai_api_key="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"
+            openai_api_key="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"
         )
 
         # Define the prompt template
